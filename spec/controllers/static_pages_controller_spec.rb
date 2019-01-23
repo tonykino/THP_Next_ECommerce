105284479a780fa002fd90afb3653d146e180e91
Tony StarrK <tony.kino.3@gmail.com>
2019-01-23 15:18:00 +0100

    Successfull CI merge on master should be auto deployed on heroku

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe StaticPagesController, type: :controller do
   describe "GET #hello" do
     it "returns http success" do
       get :hello
-      expect(response).to have_http_status(:error)
+      expect(response).to have_http_status(:success)
     end
   end
 end
