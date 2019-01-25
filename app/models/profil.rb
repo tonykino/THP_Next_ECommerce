f48b3689d316fc95f1f2292c376d68fd3ee19e4a
Emna Guellaty <eguellaty@gmail.com>
2019-01-25 19:49:55 +0100

    sidekiq config

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: profils
+#
+#  id           :bigint(8)        not null, primary key
+#  first_name   :string
+#  last_name    :string
+#  phone_number :string
+#  user_id      :bigint(8)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Profil < ApplicationRecord
   belongs_to :user
 end
