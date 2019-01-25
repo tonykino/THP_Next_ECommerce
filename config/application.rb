871b710b205883b1c0e5c1d0a09e215e992c5b98
Félix Perez <felix2@hotmail.fr>
2019-01-25 13:47:41 +0100

    Switch devise's language from English to French

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ECommerce
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = :fr
     config.generators do |g|
       g.system_tests = nil
       g.assets false
