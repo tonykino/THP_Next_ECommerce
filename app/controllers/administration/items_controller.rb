9c07b4362a8bcc4ac8c6696650650bf6a77556a8
Emna Guellaty <eguellaty@gmail.com>
2019-01-29 23:42:02 +0100

    dashboard admin

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -2,8 +2,43 @@
 
 module Administration
   class ItemsController < AdministrationController
+    before_action :set_item, only: %i[edit show update destroy]
     def index
-      @items = Item.all
+      @items = Item.all.order(:price)
     end
+
+    def show; end
+
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_params)
+      @item.save
+      redirect_to administration_items_path
+    end
+
+    def edit; end
+
+    def update
+      @item.update(item_params)
+      redirect_to administration_items_path
+    end
+
+    def destroy
+      @item.destroy
+      redirect_to administration_items_path
+    end
+
+    private
+
+      def set_item
+        @item = Item.find(params[:id])
+      end
+
+      def item_params
+        params.require(:item).permit(:name, :description, :price )
+      end
   end
 end
