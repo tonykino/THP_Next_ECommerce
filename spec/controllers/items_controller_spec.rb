c7a1a68c1dd515d4896e6d91c4e41cf3b4a9339a
Emna Guellaty <eguellaty@gmail.com>
2019-01-29 16:01:30 +0100

    itemshowindex

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, params: { id: item.id }
+    end
+    let(:item) { Item.create(name: 'Black tea', description: 'whowh', price: 12) }
+  end
 end
