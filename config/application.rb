557fd5737348e52233d7e0e55e80954d9c144624
Félix Perez <felix2@hotmail.fr>
2019-01-30 19:56:55 +0100

    Order History

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module ECommerce
     # Initialize configuration defaults for originally generated Rails version.
     config.active_job.queue_adapter = :sidekiq
     config.load_defaults 5.2
-    config.i18n.default_locale = :fr
+    config.i18n.default_locale = :en
     config.generators do |g|
       g.system_tests = nil
       g.assets false
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'orders/show', to: 'orders#show'
+  # get 'orders/index', to: 'orders#index'
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
   root 'items#index'
   resources :items, only: %i[show]
-
+  resources :orders, only: %i[index show]
   devise_for :users
 
   namespace 'administration' do
