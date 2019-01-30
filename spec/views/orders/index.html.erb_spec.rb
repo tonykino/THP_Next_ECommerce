abfa499cf14e3af211dcd83145fbb544a0ff0a09
Félix Perez <felix2@hotmail.fr>
2019-01-30 20:18:29 +0100

    Delete some pending tests

--- spec/views/orders/index.html.erb_spec.rb
+++ spec/views/orders/index.html.erb_spec.rb
@@ -2,6 +2,4 @@
 
 require 'rails_helper'
 
-RSpec.describe "orders/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe "orders/index.html.erb", type: :view do; end
