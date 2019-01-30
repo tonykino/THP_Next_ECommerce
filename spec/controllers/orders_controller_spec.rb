c71bd054f3e1a43f31de2ff225bed993edecc5c5
Félix Perez <felix2@hotmail.fr>
2019-01-30 20:15:54 +0100

    Add and fix tests for OrdersController

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe OrdersController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
+      user = create(:user)
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -12,7 +14,10 @@ RSpec.describe OrdersController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      @user = create(:user)
+      sign_in @user
+      @order = Order.create(total: "22", user_id: @user.id)
+      get :show, params: { id: @order.id }
       expect(response).to have_http_status(:success)
     end
   end
