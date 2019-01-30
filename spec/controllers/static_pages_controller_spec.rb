d8b490576239d45c1ed3cdd5a4f9c4f666cc7e3d
Tony StarrK <tony.kino.3@gmail.com>
2019-01-30 13:49:53 +0100

    Delete useless hello spec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,10 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  describe "GET #hello" do
-    it "returns http success" do
-      get :hello
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
