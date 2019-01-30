71e89b01656b6402a92dae5a5c73eaead6ad9941
Tony StarrK <tony.kino.3@gmail.com>
2019-01-30 01:08:21 +0100

    Create OrderItem model and make associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,5 +12,8 @@
 #
 
 class Order < ApplicationRecord
+  has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items
+
   enum status: %i[biding validated treated]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_233746) do
+ActiveRecord::Schema.define(version: 2019_01_29_235317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 2019_01_29_233746) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "order_id"
+    t.integer "quantity", default: 1
+    t.decimal "subtotal", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -72,5 +83,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_233746) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "profiles", "users"
 end
