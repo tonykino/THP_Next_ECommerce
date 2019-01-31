f236f9de9a4b508068f5c91b952d27a8c7fe0457
Tony StarrK <tony.kino.3@gmail.com>
2019-02-01 00:59:45 +0100

    Refactor item model specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,7 +15,33 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe 'Item Database' do
+  subject do
+    build(:item)
+  end
+
+  describe 'is creatable' do
+    let(:item) { create(:item) }
+    it { expect(item.id).not_to be_nil }
+    it { expect(item.name).not_to be_nil }
+  end
+
+  describe 'with orders' do
+    let!(:item) { create(:item, :with_orders) }
+    it { expect(item.orders).not_to be_empty }
+    it { expect(item.orders.first.id).not_to be_nil }
+    it { expect(item.orders.first.items).to include(item) }
+
+    it 'destroys its associated order_items upon destruction' do
+      expect{ item.destroy }.to change(OrderItem, :count).from(item.orders.count).to(0)
+    end
+
+    it 'does not destroy associated items upon destruction' do
+      expect{ item.destroy }.to_not change(Order, :count)
+    end
+  end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
     it { is_expected.to have_db_column(:name).of_type(:string) }
     it { is_expected.to have_db_column(:description).of_type(:text) }
     it { is_expected.to have_db_column(:price).of_type(:decimal) }
@@ -23,15 +49,11 @@ RSpec.describe Item, type: :model do
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
   end
 
-  describe 'validations' do
+  describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:price) }
     it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(40) }
     it { is_expected.to validate_length_of(:description).is_at_least(2).is_at_most(400) }
-  end
-
-  it 'is creatable' do
-    expect{ create(:item) }.to change(Item, :count).by(1)
   end
 end
