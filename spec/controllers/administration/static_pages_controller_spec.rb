b435651a22280ef704adbe0d53d37cc5a2c7dff7
Tony StarrK <tony.kino.3@gmail.com>
2019-02-01 17:37:04 +0100

    Remove useless spec

--- spec/controllers/administration/static_pages_controller_spec.rb
+++ spec/controllers/administration/static_pages_controller_spec.rb
@@ -3,10 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe Administration::StaticPagesController, type: :controller do
-  describe "GET #hello_admin" do
-    it "returns http success" do
-      get :hello_admin
-      expect(response).to have_http_status(:found)
-    end
-  end
 end
