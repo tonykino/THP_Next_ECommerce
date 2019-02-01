4f711ff280b30466da54255d85aa2ac3dea52ca3
Tony StarrK <tony.kino.3@gmail.com>
2019-02-01 01:00:57 +0100

    Refactor order_item model specs

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -16,5 +16,36 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    build(:order_item)
+  end
+
+  describe 'is creatable' do
+    let(:order_item) { create(:order_item) }
+    it { expect(order_item.id).not_to be_nil }
+    it { expect(order_item.quantity).not_to be_nil }
+  end
+
+  describe "does not destroy 'parents' upon destruction" do
+    let!(:order_item) { create(:order_item) }
+    it { expect{ order_item.destroy }.to_not change(Order, :count) }
+    it { expect{ order_item.destroy }.to_not change(Item, :count) }
+  end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:item_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:order_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:quantity).of_type(:integer).with_options(default: 1) }
+    it { is_expected.to have_db_column(:subtotal).of_type(:decimal).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
+  describe 'Validations' do
+    # it { is_expected.to validate_presence_of(:quantity) }
+    # it { is_expected.to validate_numericality_of(:quantity).only_integer.is_greater_than_or_equal_to(0) }
+    # it { is_expected.to validate_presence_of(:subtotal) }
+    # it { is_expected.to validate_numericality_of(:quantity) }
+  end
 end
