920ea792cd74051783c2a91d92dc1ca2cca9bb96
Tony StarrK <tony.kino.3@gmail.com>
2019-02-01 18:29:15 +0100

    Refactor administration::orderscontroller

--- app/controllers/administration/orders_controller.rb
+++ app/controllers/administration/orders_controller.rb
@@ -2,23 +2,31 @@
 
 module Administration
   class OrdersController < AdministrationController
-    def index
-      @orders = Order.all_except('biding').order(:status)
-    end
+    before_action :set_orders, only: %i[index]
+    before_action :set_order, only: %i[show create]
+    before_action :set_order_items, only: %i[create]
 
-    def show
-      @order = Order.find(params[:id])
-      @order_items = @order.order_items
-    end
+    def index; end
 
-    def update
-      @orders = Order.find(params[:id])
-    end
+    def show; end
 
     def create
-      @orders = Order.find(params[:order_id])
-      @orders.update(status: "treated")
+      @order.update(status: "treated")
       redirect_back fallback_location: administration_orders_path, notice: "Vous avez bien validé votre commande"
     end
+
+    private
+
+      def set_orders
+        @orders = Order.all_except('biding').order(:status)
+      end
+
+      def set_order
+        @order = Order.find(params[:id])
+      end
+
+      def set_order_items
+        @order_items = @order.order_items
+      end
   end
 end
