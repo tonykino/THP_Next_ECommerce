4494267e5b387e116722163f2fd4b349c8d2a5be
Tony StarrK <tony.kino.3@gmail.com>
2019-02-01 01:25:48 +0100

    Refactor user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,7 +21,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'User Database' do
+  subject do
+    build(:user)
+  end
+
+  describe 'is creatable' do
+    let(:user) { create(:user) }
+    it { expect(user.id).not_to be_nil }
+    it { expect(user.email).not_to be_nil }
+  end
+
+  describe 'with orders' do
+    let(:user) { create(:user, :with_orders) }
+    it { expect(user.orders).not_to be_empty }
+    it { expect(user.orders.first.id).not_to be_nil }
+    it { expect(user.orders.first.user).to eq(user) }
+
+    it 'destroys its orders upon destruction' do
+      expect{ user.destroy }.to change(Order, :count).from(user.orders.count).to(0)
+    end
+  end
+
+  describe 'with addresses' do
+    let(:user) { create(:user, :with_addresses) }
+    it { expect(user.addresses).not_to be_empty }
+    it { expect(user.addresses.first.id).not_to be_nil }
+    it { expect(user.addresses.first.user).to eq(user) }
+
+    it 'destroys its addresses upon destruction' do
+      expect{ user.destroy }.to change(Address, :count).from(user.addresses.count).to(0)
+    end
+  end
+
+  describe 'Database' do
     it { is_expected.to have_db_column(:id).of_type(:integer) }
     it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false) }
     it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(default: "", null: false) }
@@ -37,13 +69,9 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:confirmation_sent_at).of_type(:datetime) }
   end
 
-  describe 'validations' do
+  describe 'Validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
-  end
-
-  it 'is creatable' do
-    expect{ create(:user) }.to change(User, :count).by(1)
   end
 end
