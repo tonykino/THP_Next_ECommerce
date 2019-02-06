f6c0bdb66ff3402e8198b933b7c6db94f7a49f25
perezfelix <felix2@hotmail.fr>
2019-02-06 19:14:56 +0100

    Fix the method in mailer, and remove the pendings tests

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -2,6 +2,4 @@
 
 require "rails_helper"
 
-RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe OrderMailer, type: :mailer do; end
